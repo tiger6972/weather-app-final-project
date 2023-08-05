a6d758292fd0b614d68d8d84893f4f7f9f8a157a
tiger6972 <140623267+tiger6972@users.noreply.github.com>
2023-08-05 22:34:19 +0100

    js date improvement css <a> improvements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
