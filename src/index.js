2b0cc90827880b22ce8549a88a3cd8aaf344adc6
tiger6972 <140623267+tiger6972@users.noreply.github.com>
2023-08-05 19:39:41 +0100

    js date fucntion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#today-temperature");
@@ -5,17 +23,17 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector(`#description`);
   let humidityElement = document.querySelector(`#humidity`);
   let windElement = document.querySelector(`#wind-speed`);
-  let iconElement = document.querySelector(`#icon`);
+  let dateElement = document.querySelector(`#currentDate`);
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  iconElement.innerHTML = response.data.condition.icon;
+  dateElement.innerHTML = formatDate(response.data.time);
 }
 
 let apiKey = "05d7e4b8cc452aecc8t7e30oc08edef4";
 let unit = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=${unit}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Frankfurt&key=${apiKey}&units=${unit}`;
 
 axios.get(apiUrl).then(displayTemperature);
